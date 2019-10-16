f5e74a9b535c6c4dd128fab98b12b5cafa74a23d
NONstop5 <non_2002@mail.ru>
2019-10-16 19:41:14 +0300

    Add default layout

--- src/App/Views/Layouts/default.php
+++ src/App/Views/Layouts/default.php
@@ -1,1 +1,22 @@
-<h1>Layouts->default</h1>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="/libs/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Default</title>
+</head>
+<body>
+<h1>Default layout</h1>
+<?php echo $content ?>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+<script src="/libs/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+</body>
+</html>
