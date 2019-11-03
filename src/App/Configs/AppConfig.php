fafa3c214b52ae5c9267d522c40803d417a53bef
NONstop5 <non_2002@mail.ru>
2019-11-04 00:57:35 +0300

    Refactoring AppConfig class

--- src/App/Configs/AppConfig.php
+++ src/App/Configs/AppConfig.php
@@ -11,17 +11,13 @@ class AppConfig
     const APP_ROOT = 'APP_ROOT';
     const CONTROLLER_PATH = 'CONTROLLER_PATH';
     const MODEL_PATH = 'MODEL_PATH';
-    const VIEW_PATH = 'VIEW_PATH';
-    const LAYOUTS_PATH = 'LAYOUTS_PATH';
-    const DEFAULT_LAYOUT_NAME = 'DEFAULT_LAYOUT_NAME';
+    const DEFAULT_HTML_PAGE_NAME = 'DEFAULT_HTML_PAGE_NAME';
     const DEFAULT_CONTROLLER_NAME = 'DEFAULT_CONTROLLER_NAME';
     const DEFAULT_ACTION_NAME = 'DEFAULT_ACTION_NAME';
-    const DEFAULT_VIEW_NAME = 'DEFAULT_VIEW_NAME';
 
     const ACTION_POSTFIX = 'ACTION_POSTFIX';
-    const VIEW_EXTENSION = 'VIEW_EXTENSION';
 
-    public static function getDbConfig()
+    public function getDbConfig()
     {
         return [
             'dbDriver' => 'mysql',
@@ -34,19 +30,16 @@ class AppConfig
         ];
     }
 
-    public static function getAppConfig()
+    public function getAppConfig()
     {
         $appConfig[self::PUBLIC_ROOT] = $_SERVER['DOCUMENT_ROOT'];
         $appConfig[self::ROOT] = dirname($appConfig[self::PUBLIC_ROOT]);
         $appConfig[self::APP_ROOT] = $appConfig[self::ROOT] . '/src/App/';
         $appConfig[self::CONTROLLER_PATH] = $appConfig[self::APP_ROOT] . 'Controllers/';
         $appConfig[self::MODEL_PATH] = $appConfig[self::APP_ROOT] . 'Models/';
-        $appConfig[self::VIEW_PATH] = $appConfig[self::APP_ROOT] . 'Views/';
-        $appConfig[self::LAYOUTS_PATH] = $appConfig[self::VIEW_PATH] . 'Layouts/';
-        $appConfig[self::DEFAULT_LAYOUT_NAME] = 'defaultLayout';
+        $appConfig[self::DEFAULT_HTML_PAGE_NAME] = 'htmlPage';
         $appConfig[self::DEFAULT_CONTROLLER_NAME] = 'Main';
         $appConfig[self::DEFAULT_ACTION_NAME] = 'index';
-        $appConfig[self::DEFAULT_VIEW_NAME] = 'index';
         $appConfig[self::ACTION_POSTFIX] = 'Action';
 
         return $appConfig;
