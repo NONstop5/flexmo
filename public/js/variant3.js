bea93ed836a8f348d47063632093e8b93ece1059
NONstop5 <non_2002@mail.ru>
2019-11-09 06:57:33 +0300

    3-й вариант - рефакторинг вывода данных

--- public/js/variant3.js
+++ public/js/variant3.js
@@ -1,28 +1,20 @@
 (function () {
   const $buttonGetData = $(`.button-get-data`);
-  const $treeView = $(`#treeview`);
+  const $tree = $(`#tree`);
 
   /**
    * Создает и отрисовывает дерево по переданным данным
    * @param data
    */
   const generateTree = (data) => {
-    let htmlResult = ``;
-    data.forEach((dataItem) => {
-      let levels = dataItem.position.split(`.`);
-      let levelString = levels.reduce((result, item) => {
-        return result + `-`;
-      }, '');
-
-      htmlResult += `<li>${levelString}<b>${dataItem.position}</b> - <i>${dataItem.title}</i> - <u>${dataItem.value}</u></li>\n`;
-    });
-
-    //$treeView.html(htmlResult);
-    $treeView.hummingbird('addNode', {
-      pos: 'after', anchor_attr: 'text', anchor_name: 'Joe Pesci',
-      text: 'Ray Liotta', the_id: 'Ray', data_id: 'Ray', end_node: true
-    });
-    $treeView.hummingbird();
+    let htmlResult = data.reduce((result, dataItem) => {
+      return result + `
+        <li data-tree-branch="${dataItem.position}">
+            <span data-tree-click="${dataItem.position}">${dataItem.position} - ${dataItem.title} - ${dataItem.value}</span>
+        </li>
+      `;
+    }, '');
+    $tree.html(htmlResult);
   };
 
   /**
@@ -35,6 +27,8 @@
       dataType: `json`,
       success(response) {
         generateTree(response.data);
+        $buttonGetData.prop('disabled', true);
+        $tree.dataTree();
       },
       error() {
         throw new Error(`Ошибка при обращении к серверу!`);
@@ -44,9 +38,6 @@
 
   // При нажатии накнопку получения данных из БД
   $buttonGetData.on(`click`, () => {
-    //getAjaxData();
-
-    //$treeView.hummingbird();
-    console.log($treeView);
+    getAjaxData();
   });
 })();
--- src/App/Components/Tree/template.php
+++ src/App/Components/Tree/template.php
@@ -31,21 +31,7 @@
                 </button>
             </div>
             <!-- TODO Вариант на JS, с использованием JQuery-библиотеки, просто для разнообразия -->
-            <div class="hummingbird-treeview-converter">
-                <li>Warner Bros.</li>
-                <li>-Goodfellas</li>
-                <li>--Robert De Niro</li>
-                <li>--Joe Pesci</li>
-                <li>-The Shawshank Redemption</li>
-                <li>--Tim Robbins</li>
-                <li>--Morgan Freeman</li>
-                <li>Paramount</li>
-                <li>-The Untouchables</li>
-                <li>--Robert De Niro</li>
-                <li>--Kevin Costner</li>
-                <li>-Forrest Gump</li>
-                <li>--Tom Hanks</li>
-                <li>--Robin Wright</li>
+            <div id="tree">
             </div>
         </div>
     </div>
