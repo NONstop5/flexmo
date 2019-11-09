4a028cc19b59794169a4c3c08ca49e102c691459
NONstop5 <non_2002@mail.ru>
2019-11-09 06:53:51 +0300

    Заменил JQuery-библиотеку

--- src/App/Components/Layouts/HtmlPage/template.php
+++ src/App/Components/Layouts/HtmlPage/template.php
@@ -6,7 +6,7 @@
 
     <link href="/libs/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="/libs/font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet">
-    <link href="/libs/hummingbird-treeview/hummingbird-treeview.css" rel="stylesheet">
+    <link href="/libs/data-tree/style.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
 
     <title><?php echo $pageTitle ?></title>
@@ -18,7 +18,7 @@
 <script src="/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="/libs/popper/1.14.6/popper.min.js"></script>
 <script src="/libs/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-<script src="/libs/hummingbird-treeview/hummingbird-treeview.js"></script>
+<script src="/libs/data-tree/jquery.dataTree.min.js"></script>
 <script src="/js/script.js"></script>
 <script src="/js/variant3.js"></script>
 </body>
